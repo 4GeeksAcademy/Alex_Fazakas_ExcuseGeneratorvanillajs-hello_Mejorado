293e55610279ef83112ff9cdaa01b9708270c342
Alex Fazakas <144437816+alexcf04@users.noreply.github.com>
2023-11-14 20:16:12 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = Math.random();
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+let randomwho = Math.random;
+let randomaction = Math.random;
+let randomwhat = Math.random;
+let randomwhen = Math.random;
